3609adf610e9fe419f45cf1b990ba922870f24fa
GoldenF141 <67724186+GoldenF141@users.noreply.github.com>
2023-06-26 02:17:53 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -117,28 +117,6 @@ export default (() => {
 				</Text>
 			</Box>
 		</Section>
-		<Link
-			font={"--capture"}
-			font-size={"10px"}
-			position={"fixed"}
-			bottom={"12px"}
-			right={"12px"}
-			z-index={"4"}
-			border-radius={"4px"}
-			padding={"5px 12px 4px"}
-			background-color={"--dark"}
-			opacity={"0.6"}
-			hover-opacity={"1"}
-			color={"--light"}
-			cursor={"pointer"}
-			transition={"--opacityOut"}
-			quarkly-title={"Badge"}
-			text-decoration-line={"initial"}
-			href={"https://quarkly.io/"}
-			target={"_blank"}
-		>
-			Made on Quarkly
-		</Link>
 		<RawHtml>
 			<style place={"endOfHead"} rawKey={"649820fc477c3a0019b3f1d4"}>
 				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
